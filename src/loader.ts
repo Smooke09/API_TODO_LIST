5cb98bae1e62b1fdd8598af420ce235f86cde6ae
Smooke09 <pedrolucas1014zz@gmail.com>
2022-10-09 16:47:19 -0300

    ajust port project

--- src/loader.ts
+++ src/loader.ts
@@ -2,6 +2,6 @@
 import server from "./config/server";
 
 // Iniciando o Servidor
-server.listen(3333, () => {
+server.listen(process.env.SERVER_PORT || 3333, () => {
   console.log("Server is running! 🚀");
 });
